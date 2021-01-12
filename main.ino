3a96a85a84cf1b5c8b17f0cea1ffd6307f607d10
uplukaszp <uplukaszp1@gmail.com>
2021-01-12 16:23:35 +0100

    Added encoder and switch support

--- main.ino
+++ main.ino
@@ -1,4 +1,6 @@
 #include <LiquidCrystal.h>
+#include <Encoder.h>
+
 #include "InputSwitches.h"
 #include "Enumerations.h"
 
@@ -8,19 +10,71 @@
 #define LCD_D7 PIN_A0
 #define LCD_RS PIN_A2
 #define LCD_EN PIN_A3
+#define SW 3
+#define CLK 2
+#define DT 5
 
 InputSwitches switches;
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+Encoder encoder(CLK, DT);
+
 SwitchState s;
 uint8_t distance = 0;
 uint8_t feed = 0;
-
+int32_t lastPos;
+boolean lastIsRunning = false;
+boolean isRunning = false;
+unsigned last_interrupt_time = millis();
+void handleClick()
+{
+    unsigned long interrupt_time = millis();
+    if (interrupt_time - last_interrupt_time > 1000)
+    {
+        if (isRunning)
+            isRunning = false;
+        else
+            isRunning = true;
+        Serial.println(isRunning);
+        if (isRunning)
+        {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("START!");
+        }
+    }
+    last_interrupt_time = interrupt_time;
+}
 void setup()
 {
     Serial.begin(9600);
     while (!Serial)
         ;
     lcd.begin(16, 2);
+    lastPos = encoder.read();
+    pinMode(SW, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(SW), handleClick, LOW);
+}
+uint32_t readValueToAdd()
+{
+    int32_t currentPos = encoder.read();
+    uint8_t valueToAdd = (currentPos == lastPos) ? 0 : (currentPos > lastPos) ? 1 : -1;
+    lastPos = currentPos;
+    return valueToAdd;
+}
+void updateValues()
+{
+    uint8_t valueToAdd = readValueToAdd();
+    switch (s.direction)
+    {
+    case Direction::Feed:
+        feed += valueToAdd;
+        break;
+    case Direction::Distance:
+        distance += valueToAdd;
+        break;
+    default:
+        break;
+    }
 }
 
 void printMode()
@@ -88,13 +142,18 @@ void printSettings()
 }
 void printMenu()
 {
-    printDirection();
-    printMode();
-    printSettings();
+    if (!isRunning)
+    {
+        printDirection();
+        printMode();
+        printSettings();
+    }
 }
 
 void loop()
 {
     s = switches.getCurrentState();
+    updateValues();
     printMenu();
+    delay(50);
 }
